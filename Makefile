b50484cdb17807e645d5a6183be641dfc7546e33
Chris G <cgeorgiades27@gmail.com>
2018-09-30 18:16:26 -0400

    Completed decoding function.  Will need to package into a method

--- Makefile
+++ Makefile
@@ -2,15 +2,15 @@ CPP = g++
 DEBUG = -O0 -g
 C11 = -std=c++11
 
-all: decode2.exe clean
+all: decode3.exe clean
 
-decode2.exe: decode-2.o
+decode3.exe: decode-3.o
 	@echo "Creating..."
 	$(CPP) -o $@ $< $(C11)
 
-decode-2.o: decode-2.cpp 
+decode-3.o: decode-3.cpp 
 	@echo "Checking..."
-	$(CPP) $(DEBUG) -c decode-2.cpp $(C11)
+	$(CPP) $(DEBUG) -c decode-3.cpp $(C11)
 
 clean:
 	@echo "Cleaning..."
---
+++ decode-3.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+
+#define MAXLINE 80
+
+char line[MAXLINE];
+unsigned int funct_, shamt_, rd_, rs_, rt_, opcode_, addr_, immed_;
+unsigned int instruction;
+
+void printR(unsigned int opcode, unsigned int rs, unsigned int rt, unsigned int rd, unsigned int shamt, unsigned int funct);
+void printI(unsigned int opcode, unsigned int rs, unsigned int rt, unsigned int immed);
+void printJ(unsigned int opcode, unsigned int addr);
+
+int main()
+{
+  
+  while(fgets(line, MAXLINE, stdin))
+    {
+      sscanf(line, "%x", &instruction);
+      printf("instruction parsed: %x\n", instruction);
+
+      // decode opcode first
+      opcode_ = instruction >> 26;
+
+      // determine instruction type...
+      if (opcode_ == 0)
+	{
+	  funct_ = instruction << 26 >> 26;
+	  shamt_ = instruction << 21 >> 27;
+	  rd_ = instruction << 16 >> 27;
+	  rs_ = instruction << 11 >> 27;
+	  rt_ = instruction << 6 >> 27;
+	  printR(opcode_, rt_, rt_, rd_, shamt_, funct_);
+	}
+      
+      else if (opcode_ == 2 || opcode_ == 3)
+	{
+	  addr_ = instruction << 7 >> 7;
+	  printJ(opcode_, addr_);
+	}
+      else
+	{
+	  rs_ = instruction << 11 >> 27;
+	  rt_ = instruction << 6 >> 27;
+	  immed_ = instruction << 16 >> 16;
+	  printI(opcode_, rs_, rt_, immed_);
+	}
+    } 
+  return 0;
+} // end main
+
+void printR(unsigned int opcode, unsigned int rs, unsigned int rt, unsigned int rd, unsigned int shamt, unsigned int funct)
+{
+  std::cout << std::setw(8) << std::left << "opcode" << std::hex << std::setw(4) << std::right << opcode << "\n"
+	    << std::setw(8) << std::left << "rs"     << std::hex << std::setw(4) << std::right << rs     << "\n"
+	    << std::setw(8) << std::left << "rt"     << std::hex << std::setw(4) << std::right << rt     << "\n"
+	    << std::setw(8) << std::left << "rd"     << std::hex << std::setw(4) << std::right << rd     << "\n"
+	    << std::setw(8) << std::left << "shamt"  << std::hex << std::setw(4) << std::right << shamt  << "\n"
+	    << std::setw(8) << std::left << "funct"  << std::hex << std::setw(4) << std::right << funct  << "\n";
+}
+
+void printI(unsigned int opcode, unsigned int rs, unsigned int rt, unsigned int immed)
+{
+  std::cout << std::setw(8) << std::left << "opcode" << std::hex << std::setw(4) << std::right << opcode << "\n"
+	    << std::setw(8) << std::left << "rs"     << std::hex << std::setw(4) << std::right << rs     << "\n"
+	    << std::setw(8) << std::left << "rt"     << std::hex << std::setw(4) << std::right << rt     << "\n"
+	    << std::setw(8) << std::left << "immed"  << std::hex << std::setw(4) << std::right << immed  << "\n";
+}
+
+void printJ(unsigned int opcode, unsigned int addr)
+{
+  std::cout << std::setw(8) << std::left << "opcode" << std::hex << std::setw(4) << std::right << opcode << "\n"
+	    << std::setw(8) << std::left << "addr"     << std::hex << std::setw(4) << std::right << addr << "\n";
+}
+
+
